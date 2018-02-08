91bbbe07a039c2878db68bf95427463a0467e88c
Brian Garcia <brian.garcia@student.uncommonhs.org>
2018-02-08 23:22:24 +0000

    tried to implement vowels to change

--- js/script.js
+++ js/script.js
@@ -5,23 +5,56 @@
 // CREATE THE FUNCTIONS BELOW
 
 // Document Ready Function. All of your jQuery should go in here. 
-$( document ).ready(function() {
-  
+function WordToPigLatin(word){
+	if (word.length == 1)
+	
+	{var vowels = new Array('a','e','i','o','u');
+    var isVowel = false;
+
+    for(e in vowels)
+    {
+      if(vowels[e] == word)
+      {
+        isVowel = true;
+      }
+    }
+
+    return isVowel;
+  }
+	word.slice(-1);
+	return word + "ay";
+}
+}
+WordToPigLatin(" ");
 
 
 
+function sentenceToPigLatin(sentence) {
+	var word = sentence.split(' ');
+	for (var i=0; i<word.length;i++);{
+	word[i]=sentenceToPigLatin(word[i]);
+	return word.join(' ');
+	}
+}
+
+$(document).ready(function() {
+	$("#button").click(function() {
+		var sentence = $("#input").val();
+
+
+console.log(sentence);
+	});
 });
 
 
-// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
+
+	// Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
 
 
 
 
 
-// Create the sentenceToPigLatin function that takes a sentence as a parameter
+	// Create the sentenceToPigLatin function that takes a sentence as a parameter
 	//Loops through all the words in the sentence and transforms each word
 	//It should return a transfromed sentance
-
-
